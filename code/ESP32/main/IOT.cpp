82f4cec410df01cdb45f2d1edab1b2bb4699410e
Graham Ross <graham.a.ross@gmail.com>
2025-12-30 05:16:08 -0500

    Clear handle before MQTT reconnect

--- code/ESP32/main/IOT.cpp
+++ code/ESP32/main/IOT.cpp
@@ -51,9 +51,9 @@ void IOT::Init(IOTCallbackInterface *iotCB, AsyncWebServer *pwebServer) {
            OUTPUT); // use LED for wifi AP status (note:edgeBox shares the LED pin with the serial TX gpio)
 #endif
    pinMode(GPIO_NUM_0, INPUT_PULLUP);
+   EEPROM.begin(EEPROM_SIZE);
 #ifdef FACTORY_RESET_PIN // use digital input pin for factory reset
    pinMode(FACTORY_RESET_PIN, INPUT_PULLUP);
-   EEPROM.begin(EEPROM_SIZE);
    if (digitalRead(FACTORY_RESET_PIN) == LOW) {
       logi("Factory Reset");
       EEPROM.write(0, 0);
@@ -61,7 +61,6 @@ void IOT::Init(IOTCallbackInterface *iotCB, AsyncWebServer *pwebServer) {
       saveSettings();
    }
 #elif BUTTONS // use analog pin for factory reset
-   EEPROM.begin(EEPROM_SIZE);
    uint16_t analogValue = analogRead(BUTTONS);
    logd("button value (%d)", analogValue);
    if (analogValue > 3000) {
@@ -451,7 +450,7 @@ void IOT::Run() {
                   setState(Connecting);
                }
             } else {
-#if  defined(Has_OLED) || defined(Has_TFT)
+#if defined(Has_OLED) || defined(Has_TFT)
                int countdown = (AP_TIMEOUT - (millis() - _waitInAPTimeStamp)) / 1000;
                if (countdown != _lastCountdown) {
                   _lastCountdown = countdown;
@@ -570,7 +569,7 @@ void IOT::setState(NetworkState newState) {
                              : _networkState == OnLine     ? "OnLine"
                                                            : "OffLine");
 
-#if  defined(Has_OLED) || defined(Has_TFT)
+#if defined(Has_OLED) || defined(Has_TFT)
    String mode;
    String detail;
    if (_networkState == OnLine) {
